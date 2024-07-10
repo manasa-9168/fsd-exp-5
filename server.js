f4fb79b8059e0f340d12abeaed8b3d6165200129
manasa-9168 <163173068+manasa-9168@users.noreply.github.com>
2024-07-10 06:08:07 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,39 @@
+const express=require('express');
+const app=express();
+app.use(express.json());
+app.get('/',
+    (req,res)=>{
+        res.sendFile(__dirname+'/validation.html');
+    });
+app.get('/login',
+    (req,res)=>{
+        res.sendFile(__dirname+'/login.html');
+    });
+app.get('/registration',
+    (req,res)=>{
+        res.sendFile(__dirname+'/registration.html');
+    });
+app.get('/welcome',
+    (req,res)=>{
+        res.sendFile(__dirname+'/welcome.html');
+    });
+app.post('/',
+    (req,res)=>{
+        res.redirect("/")
+    });
+app.post('/login',
+    (req,res)=>{
+        res.redirect("/login")
+    });
+app.post('/registration',
+    (req,res)=>{
+        res.redirect("/registration")
+    });
+app.post('/welcome',
+    (req,res)=>{
+        res.redirect("/welcome")
+    });
+app.listen(3000,
+    ()=>{
+        console.log("http://localhost:3000");
+    });
